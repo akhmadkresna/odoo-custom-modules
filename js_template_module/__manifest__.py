754397ecb439d78461308c996e45c3e87def049b
akhmadkresna <firstyankresna@gmail.com>
2017-09-04 23:28:37 +0700

    js_module

---
+++ js_template_module/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015 ACSONE SA/NV (<http://acsone.eu>)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Export Record on Filtered Tree',
+    'version': '10.0.1.0.0',
+    "author": "Portcities",
+    "maintainer": "Portcities",
+    "website": "http://portcities.net",
+    "license": "LGPL-3",
+    'category': 'Technical Settings',
+    'depends': [
+        'web',
+    ],
+    'summary': 'Export Records on Filtered Treeview',
+    'data': [
+        'views/templates.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
