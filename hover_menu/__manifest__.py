0a2954d37dda3842b55a3498427c7e488157b42a
akhmadkresna <firstyankresna@gmail.com>
2017-10-07 01:39:12 +0700

    hover menu css

---
+++ hover_menu/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Hover menu',
+    'version': '1.0',
+    "author": "Akhmad kresna",
+    "license": "LGPL-3",
+    'category': 'web',
+    'depends': [
+        'web',
+    ],
+    'summary': 'Show submenu when parent menu is hover',
+    'data': [
+        'views/templates.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ temp/__manifest__.py
@@ -0,0 +1,18 @@
+
+
+{
+    'name': 'JS module template',
+    'version': '1.0',
+    "author": "Akhmad kresna",
+    "license": "LGPL-3",
+    'category': 'website',
+    'depends': [
+        'website',
+    ],
+    'summary': 'Hover dropdown menu',
+    'data': [
+        'views/templates.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
